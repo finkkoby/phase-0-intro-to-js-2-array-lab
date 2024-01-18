ba2e9ab85c7a5bb9aee3218066f190ea8e5d6ef6
Koby Fink <finkkoby@icloud.com>
2024-01-18 18:40:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    name = cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    name = cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat(name) {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
